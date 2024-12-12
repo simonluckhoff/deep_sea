290bc626df0a9aa401fd179de08418715052ba60
Simon Luckhoff <simonluckhoff@gmail.com>
2024-12-12 21:33:09 +0200

    js

--- app/javascript/controllers/typed_controller.js
+++ app/javascript/controllers/typed_controller.js
@@ -1,13 +1,32 @@
+// import { Controller } from "@hotwired/stimulus"
+// import Typed from "typed.js"
+
+// // Connects to data-controller="typed"
+// export default class extends Controller {
+//   static values = { strings: Array }
+//   connect() {
+//     new Typed(this.element, {
+//       strings: this.stringsValue,
+//       typeSeed: 50
+//     })
+//   }
+// }
+
+
 import { Controller } from "@hotwired/stimulus"
 import Typed from "typed.js"
 
 // Connects to data-controller="typed"
 export default class extends Controller {
   static values = { strings: Array }
+
   connect() {
+    // Ensure strings is an array of strings (e.g. handle single string case)
+    const strings = Array.isArray(this.stringsValue) ? this.stringsValue : [this.stringsValue]
+
     new Typed(this.element, {
-      strings: this.stringsValue,
-      typeSeed: 50
+      strings: strings,
+      typeSpeed: 50
     })
   }
 }
