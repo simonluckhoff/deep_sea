aa70b2a4e86fe855a2df6adabbfb589de4ab3e22
Sharon Kazadi <sharonkazadi76@gmail.com>
2024-12-19 01:04:18 +0200

    design

--- app/controllers/gears_controller.rb
+++ app/controllers/gears_controller.rb
@@ -7,6 +7,44 @@ class GearsController < ApplicationController
     else
       @gears = Gear.all
     end
+    @fantasy_addresses = [
+      "Cape Town, South Africa",
+      "Johannesburg, South Africa",
+      "Durban, South Africa",
+      "Pretoria, South Africa",
+      "Port Elizabeth, South Africa",
+      "Bloemfontein, South Africa",
+      "East London, South Africa",
+      "Kimberley, South Africa",
+      "Polokwane, South Africa",
+      "Pietermaritzburg, South Africa",
+      "Nelspruit, South Africa",
+      "George, South Africa",
+      "Rustenburg, South Africa",
+      "Stellenbosch, South Africa",
+      "Knysna, South Africa",
+      "Hermanus, South Africa",
+      "Paarl, South Africa",
+      "Franschhoek, South Africa",
+      "Plettenberg Bay, South Africa",
+      "Mossel Bay, South Africa",
+      "Oudtshoorn, South Africa",
+      "Langebaan, South Africa",
+      "Swellendam, South Africa",
+      "Somerset West, South Africa",
+      "Robertson, South Africa",
+      "Worcester, South Africa",
+      "Ceres, South Africa",
+      "Malmesbury, South Africa",
+      "Atlantis, South Africa",
+      "Saldanha, South Africa",
+      "Vredenburg, South Africa",
+      "Wellington, South Africa",
+      "Caledon, South Africa",
+      "Tulbagh, South Africa",
+      "Cape Town, South Africa",
+      "Clanwilliam, South Africa"
+    ]
   end
 
   def show
